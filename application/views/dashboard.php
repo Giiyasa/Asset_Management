43ab61e82d409ea1c81e215b3ebce21e83b77936
Giiyasa <makgilang90@gmail.com>
2023-03-31 16:47:03 +0700

    First commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,157 @@
+<div class="row justify-content-md-center">
+
+    <div class="col-xl-3 col-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Barang</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $barang; ?></div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-boxes fa-2x text-gray-500"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-6 mb-4">
+        <div class="card border-left-danger shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Departemen</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $supplier; ?></div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-500"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Stok barang</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $stok; ?></div>
+                            </div>
+                            <div class="col-auto">
+                                <div class="progress progress-sm mr-2">
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-cubes fa-2x text-gray-500"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<div class="row pt-5 mt-5 ">
+    <div class="col-md-4">
+        <div class="card shadow mb-4">
+            <div class="card-header bg-warning py-3">
+                <h6 class="m-0 font-weight-bold text-white text-center">Minimum Stock of Goods</h6>
+            </div>
+            <div class="table-responsive">
+                <table class="table mb-0 text-center table-striped table-hover table-sm">
+                    <thead>
+                        <tr>
+                            <th>Goods</th>
+                            <th>Stock</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($barang_min) :
+                            foreach ($barang_min as $b) :
+                        ?>
+                                <tr>
+                                    <td><?= $b['nama_barang']; ?></td>
+                                    <td><?= $b['stok']; ?></td>
+                                    <td>
+                                        <a href="<?= base_url('barangmasuk/add/') . $b['id_barang'] ?>" class="btn btn-warning btn-sm"><i class="fa fa-plus"></i></a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td colspan="3" class="text-center">
+                                    No minimum stock items
+                                </td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card shadow mb-4">
+            <div class="card-header bg-success py-3">
+                <h6 class="m-0 font-weight-bold text-white text-center">Riwayat Penambahan Asset</h6>
+            </div>
+            <div class="table-responsive">
+                <table class="table mb-0 table-sm table-striped table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Nama Barang</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($transaksi['barang_masuk'] as $tbm) : ?>
+                            <tr>
+                                <td><strong><?= $tbm['tanggal_masuk']; ?></strong></td>
+                                <td><?= $tbm['nama_barang']; ?></td>
+                                <td><span class="badge badge-success"><?= $tbm['jumlah_masuk']; ?></span></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card shadow mb-4">
+            <div class="card-header bg-danger py-3">
+                <h6 class="m-0 font-weight-bold text-white text-center">Riwayat Penyerahan Barang</h6>
+            </div>
+            <div class="table-responsive">
+                <table class="table mb-0 table-sm table-striped table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Nama Barang</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($transaksi['barang_keluar'] as $tbk) : ?>
+                            <tr>
+                                <td><strong><?= $tbk['tanggal_keluar']; ?></strong></td>
+                                <td><?= $tbk['nama_barang']; ?></td>
+                                <td><span class="badge badge-danger"><?= $tbk['jumlah_keluar']; ?></span></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
